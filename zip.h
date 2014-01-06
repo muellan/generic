33d162e1743e015c26cff64b0df6ef1f1f909d51
Andre Mueller <foss@andremueller-online.de>
2014-01-06 08:43:45 +0100

    + zip

---
+++ zip.h
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ *
+ * AM utilities
+ *
+ * released under MIT license
+ *
+ * 2008-2014 André Müller
+ *
+ *****************************************************************************/
+
+#ifndef AM_GENERIC_ZIP_H_
+#define AM_GENERIC_ZIP_H_
+
+#include <type_traits>
+#include <utility>
+
+
+namespace am {
+
+namespace gen {
+
+
+/*****************************************************************************
+ *
+ * @brief
+ *
+ * @return
+ *
+ *****************************************************************************/
+
+//-------------------------------------------------------------------
+template<class InputIterator1, class InputIterator2, class OutputIterator>
+inline void
+zip(InputIterator1 beg1, InputIterator1 end1,
+	InputIterator2 beg2, InputIterator2 end2, OutputIterator out)
+{
+	for(; (beg1 != end1) && (beg2 != end2); ++beg1, ++beg2) {
+		*out++ = {*beg1,*beg2};
+	}
+}
+
+
+
+}  // namespace gen
+
+}  // namespace am
+
+
+
+#endif
+
