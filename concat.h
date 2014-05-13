88a74cf7315ebacce012cb1d1d59af842e685d56
Andre Mueller <foss@andremueller-online.de>
2014-05-13 11:39:42 +0200

    * renamed 'scan' to 'act' to avoid confusion with 'scan' from functional languages like Haskell + concat for std::array concatenation

---
+++ concat.h
@@ -0,0 +1,61 @@
+/*****************************************************************************
+ *
+ * AM utilities
+ *
+ * released under MIT license
+ *
+ * 2008-2014 André Müller
+ *
+ *****************************************************************************/
+
+#ifndef AM_GENERIC_CONCAT_H_
+#define AM_GENERIC_CONCAT_H_
+
+
+#include <array>
+#include <algorithm>
+
+
+namespace am {
+namespace gen {
+
+
+//TODO am::gen::concat variadic arg list
+
+//-------------------------------------------------------------------
+template<std::size_t n, class T, std::size_t m>
+std::array<T,n+m>
+concat(std::array<T,n> a, std::array<T,m> b)
+{
+    std::array<T,n+m> res;
+
+    std::move(begin(a), end(a), begin(res));
+    std::move(begin(b), end(b), next(begin(res),n));
+
+    return res;
+}
+
+
+
+//-------------------------------------------------------------------
+template<std::size_t n, class T, std::size_t m, std::size_t k>
+std::array<T,n+m+k>
+concat(std::array<T,n> a, std::array<T,m> b, std::array<T,k> c)
+{
+    std::array<T,n+m+k> res;
+
+    std::move(begin(a), end(a), begin(res));
+    std::move(begin(b), end(b), next(begin(res),n));
+    std::move(begin(c), end(c), next(begin(res),n+m));
+
+    return res;
+}
+
+
+
+}  // namespace gen
+}  // namespace am
+
+
+
+#endif
